313b4cc8aff575fb4b00ef45d588a7e6ec57c259
Cyril Leroux <cyril0leroux@gmail.com>
2013-04-25 01:39:19 +0200

    Added a widget for media control

---
+++ src/org/es/uremote/widget/MediaWidgerProvider.java
@@ -0,0 +1,51 @@
+package org.es.uremote.widget;
+
+import java.util.Random;
+
+import org.es.uremote.R;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+/**
+ * @author Cyril Leroux
+ *
+ */
+public class MediaWidgerProvider extends AppWidgetProvider {
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+		// Get ids of all the instances of the widget
+		ComponentName widget = new ComponentName(context, MediaWidgerProvider.class);
+		int[] widgetIds = appWidgetManager.getAppWidgetIds(widget);
+
+		for (int widgetId : widgetIds) {
+
+			// Create some random data
+			int number = (new Random().nextInt(100));
+			final String data = String.valueOf(number);
+
+			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_media);
+
+			//			// Set the text
+			//			remoteViews.setTextViewText(R.id.textToUpdate, data);
+			//			remoteViews.setTextViewText(R.id.textToUpdate, data);
+			//
+			//			// Register an onClickListener
+			//			Intent intent = new Intent(context, MediaWidgerProvider.class);
+			//			intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+			//			intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+			//
+			//			PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+			//			remoteViews.setOnClickPendingIntent(R.id.textToUpdate, pendingIntent);
+			//			appWidgetManager.updateAppWidget(widgetId, remoteViews);
+		}
+	}
+}
