1659e9ff2cbd64bff230fdfe960c8de34ccd486e
Cyril Leroux <cyril0leroux@gmail.com>
2013-05-24 01:43:09 +0200

    Created Activity to register a new server

---
+++ src/org/es/uremote/CreateServer.java
@@ -0,0 +1,18 @@
+package org.es.uremote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Cyril Leroux
+ *
+ */
+public class CreateServer extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
