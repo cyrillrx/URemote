f44358463fbc6664cea4b17309b1e19b3168985d
Cyril Leroux <cyril0leroux@gmail.com>
2013-12-08 01:14:38 +0100

    Worked with subscript ans superscript in connectedDeviceDrawable class.

--- URemote/src/main/java/org/es/uremote/graphics/ConnectedDeviceDrawable.java
+++ URemote/src/main/java/org/es/uremote/graphics/ConnectedDeviceDrawable.java
@@ -13,6 +13,7 @@ import android.graphics.drawable.Drawable;
 
 import org.es.graphics.Hexagon;
 import org.es.uremote.objects.ConnectedDevice;
+import org.es.uremote.objects.ServerSetting;
 
 import java.util.Random;
 
@@ -34,14 +35,18 @@ public class ConnectedDeviceDrawable extends Drawable {
     /** Coefficient to compute the inner hexagon side. */
     private static final float INNER_HEXAGON_SIDE_COEF = 0.35f;
 
-    private final String mText;
     private final Paint mPaint;
+
+    private final String mText;
     private final Rect mTextBounds;
 
-    public ConnectedDeviceDrawable(final ConnectedDevice device) {
+    private final String mSubscript;
+    private final Rect mSubscriptBounds;
 
-        // Text : first letter of the device name
-        mText = device.getName().substring(0, 1);
+    private final String mSuperscript;
+    private final Rect mSuperscriptBounds;
+
+    public ConnectedDeviceDrawable(final ConnectedDevice device) {
 
         // Paint
         mPaint = new Paint();
@@ -50,10 +55,24 @@ public class ConnectedDeviceDrawable extends Drawable {
         mPaint.setTypeface(Typeface.DEFAULT_BOLD);
         mPaint.setStrokeWidth(STROKE_WIDTH);
 
+        // Text : first letter of the device name
+        mText           = getText(device);
+        mSubscript      = getSubscript(device);
+        mSuperscript    = getSuperscript(device);
+
         // Text bounds
         mTextBounds = new Rect();
         mPaint.setTextSize(TEXT_SIZE);
         mPaint.getTextBounds(mText, 0, mText.length(), mTextBounds);
+
+        // Text bounds
+        mSubscriptBounds = new Rect();
+        mPaint.setTextSize(SUBSCRIPT_SIZE);
+        mPaint.getTextBounds(mSubscript, 0, mSubscript.length(), mSubscriptBounds);
+
+        // Text bounds
+        mSuperscriptBounds = new Rect();
+        mPaint.getTextBounds(mSuperscript, 0, mSuperscript.length(), mSuperscriptBounds);
     }
 
     @Override
@@ -64,27 +83,25 @@ public class ConnectedDeviceDrawable extends Drawable {
 
         // Draw the text
         mPaint.setTextSize(TEXT_SIZE);
-        final float textOriginX = centerX - mTextBounds.width()  / 2;
+        final float textOriginX = centerX - mTextBounds.width()  / 2 - Math.max(mSubscriptBounds.width(), mSuperscriptBounds.width()) / 2;
         final float textOriginY = centerY + mTextBounds.height() / 2;
         canvas.drawText(mText, textOriginX, textOriginY, mPaint);
 
-        // Draw subscript
-        final String subscript = getSubscript();
-        if (subscript != null && !subscript.isEmpty()) {
-            mPaint.setTextSize(SUBSCRIPT_SIZE);
-            final float subscriptOriginX = centerX + mTextBounds.width()  / 2 + SUBSCRIPT_PADDING;
-            final float subscriptOriginY = centerY + mTextBounds.height() / 2 + SUBSCRIPT_SIZE;
-            canvas.drawText(subscript, subscriptOriginX, subscriptOriginY, mPaint);
-        }
-
-        // Draw superscript
-        final String superscript = getSuperscript();
-        if (superscript != null && !superscript.isEmpty()) {
-            mPaint.setTextSize(SUBSCRIPT_SIZE);
-            final float superscriptOriginX = centerX + mTextBounds.width()  / 2 + SUBSCRIPT_PADDING;
-            final float superscriptOriginY = centerY - mTextBounds.height() / 2;
-            canvas.drawText(superscript, superscriptOriginX, superscriptOriginY, mPaint);
-        }
+//        // Draw subscript
+//        if (!mSubscript.isEmpty()) {
+//            mPaint.setTextSize(SUBSCRIPT_SIZE);
+//            final float subscriptOriginX = textOriginX + mTextBounds.width() + SUBSCRIPT_PADDING;
+//            final float subscriptOriginY = textOriginY + mTextBounds.height() + SUBSCRIPT_SIZE;
+//            canvas.drawText(mSubscript, subscriptOriginX, subscriptOriginY, mPaint);
+//        }
+//
+//        // Draw superscript
+//        if (!mSuperscript.isEmpty()) {
+//            mPaint.setTextSize(SUBSCRIPT_SIZE);
+//            final float superscriptOriginX = textOriginX + mTextBounds.width() + SUBSCRIPT_PADDING;
+//            final float superscriptOriginY = textOriginY - mTextBounds.height();
+//            canvas.drawText(mSuperscript, superscriptOriginX, superscriptOriginY, mPaint);
+//        }
 
         // Draw the outer hexagon
         final float outerHexagonSide = canvas.getHeight() * OUTER_HEXAGON_SIDE_COEF;
@@ -115,11 +132,23 @@ public class ConnectedDeviceDrawable extends Drawable {
         return PixelFormat.TRANSLUCENT;
     }
 
-    protected String getSubscript() {
-        return null;
+
+    protected String getText(final ConnectedDevice device) {
+        return device.getName().substring(0, 1);
     }
 
-    protected String getSuperscript() {
-        return null;
+    protected String getSubscript(final ConnectedDevice device) {
+
+//        if (device instanceof ServerSetting) {
+//            String ip = ((ServerSetting) device).getLocalHost();
+//            final int pos = ip.lastIndexOf('.');
+//            return ip.substring(pos+1);
+//        }
+
+        return "1";
+    }
+
+    protected String getSuperscript(final ConnectedDevice device) {
+        return "";
     }
 }
