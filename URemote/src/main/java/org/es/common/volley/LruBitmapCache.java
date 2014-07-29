86ee8323af77012ba27a7dc1a24661010779e299
Cyril Leroux <cyril0leroux@gmail.com>
2014-07-29 23:29:17 +0200

    Implemented volley library

---
+++ URemote/src/main/java/org/es/common/volley/LruBitmapCache.java
@@ -0,0 +1,41 @@
+package org.es.common.volley;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader;
+
+/**
+ * @author Cyril Leroux
+ *         Created 23/07/2014.
+ */
+public class LruBitmapCache extends LruCache<String, Bitmap> implements ImageLoader.ImageCache {
+
+    public static int getDefaultLruCacheSize() {
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        return maxMemory / 8;
+    }
+
+    public LruBitmapCache() {
+        this(getDefaultLruCacheSize());
+    }
+
+    public LruBitmapCache(int sizeInKiloBytes) {
+        super(sizeInKiloBytes);
+    }
+
+    @Override
+    protected int sizeOf(String key, Bitmap value) {
+        return value.getRowBytes() * value.getHeight() / 1024;
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+        put(url, bitmap);
+    }
+}
---
+++ URemote/src/main/java/org/es/uremote/URemote.java
@@ -0,0 +1,55 @@
+package org.es.uremote;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+import org.es.common.volley.LruBitmapCache;
+
+/**
+ * @author Cyril Leroux
+ *         Created 23/07/2014.
+ */
+public class URemote extends Application {
+
+    private static final String TAG = URemote.class.getSimpleName();
+
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        getRequestQueue();
+        if (mImageLoader == null) {
+            mImageLoader = new ImageLoader(this.mRequestQueue, new LruBitmapCache());
+        }
+        return this.mImageLoader;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
